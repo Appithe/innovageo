b533efad9a053baf94e6a122260ad845669b9fee
joel <joelaviles413@gmail.com>
2022-01-26 23:03:10 -0600

    Rebuild app

---
+++ build/precache-manifest.6f7c3e94c1a737d6152a3f359f3ca377.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb9f063e5da957a009fdc3cd71b0e74",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ba8909ff25fe24a6a927",
+    "url": "/my-app/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "1e0024b023a70933b52f",
+    "url": "/my-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ba8909ff25fe24a6a927",
+    "url": "/my-app/static/js/2.feaec089.chunk.js"
+  },
+  {
+    "revision": "aa0eeaaf0a009858fb2c9ec10ecbfd8f",
+    "url": "/my-app/static/js/2.feaec089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2e6497ccc0e687a9bb",
+    "url": "/my-app/static/js/3.b316c183.chunk.js"
+  },
+  {
+    "revision": "1e0024b023a70933b52f",
+    "url": "/my-app/static/js/main.96d04526.chunk.js"
+  },
+  {
+    "revision": "ab6e4ed65768f6223328",
+    "url": "/my-app/static/js/runtime-main.1d1799f8.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea544fc7e12918182d145ed7947cb256.js"
+  "/my-app/precache-manifest.6f7c3e94c1a737d6152a3f359f3ca377.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
