74b9f8e66a981bcfb412d1ccf91a5649f8d24cd8
joel <joelaviles413@gmail.com>
2022-01-26 22:35:19 -0600

    build app v1

---
+++ build/precache-manifest.ea544fc7e12918182d145ed7947cb256.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c53c9e6e1b4d0f7eece58d376dba6ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9738b9f3e7cee09182db",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "1e0024b023a70933b52f",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9738b9f3e7cee09182db",
+    "url": "/static/js/2.a8303ed9.chunk.js"
+  },
+  {
+    "revision": "aa0eeaaf0a009858fb2c9ec10ecbfd8f",
+    "url": "/static/js/2.a8303ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2e6497ccc0e687a9bb",
+    "url": "/static/js/3.b316c183.chunk.js"
+  },
+  {
+    "revision": "1e0024b023a70933b52f",
+    "url": "/static/js/main.96d04526.chunk.js"
+  },
+  {
+    "revision": "46919ea6f832cc6248dc",
+    "url": "/static/js/runtime-main.821af257.js"
+  }
+]);
